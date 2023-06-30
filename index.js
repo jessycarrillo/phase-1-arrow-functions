a827cdecb7d590a952fd89dd700618533fd7887f
Jessica Carrillo <jessymamo@gmail.com>
2023-06-30 14:31:01 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1) has a fFunction expression called divide
+  //2) divide divides 2000 by 100
+    //3) has an arrow function called square
+    //4) square arrow function takes one parameter and multiplies it times itself
+    //5) has an arrow function called add
+    // 6) add arrow function takes two parameters and adds them together
+
+    const divide = (a, b) => a / b; 
+    
+    const square = (a) => a * a;
+
+    const add = (a, b) => a + b;
